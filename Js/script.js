b7e820174377fc8e860686a532d017174879ad44
Md saheb <sahebbca2020@gmail.com>
2022-12-05 00:20:50 +0530

    write some logic to turn on and off dark mode

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+let light = document.getElementById('darkLight');
+let visualMode = document.getElementById("Mode");
+light.addEventListener('click',dothis);
+function dothis()
+{
+    if(light.style.justifyContent === "start")
+    {
+        light.style.justifyContent="end";
+        visualMode.classList.remove("fa-moon");
+        visualMode.classList.add("fa-sun");
+    }
+    else
+    {
+        light.style.justifyContent="start";
+        visualMode.classList.remove("fa-sun");
+        visualMode.classList.add("fa-moon");
+    }
+}
